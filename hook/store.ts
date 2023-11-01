8232291cb006fc9eea9d9d54aa2f30cd8e84b39a
rieliss <133730089+rieliss@users.noreply.github.com>
2023-11-01 15:15:55 +0700

    1.0.1

--- hook/store.ts
+++ hook/store.ts
@@ -1,27 +1,27 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-type StoreState = {
-  price: number;
-  SumPrice: (value: number) => void;
-  MinusPrice: (value: number) => void;
-  CheckAll: boolean;
-  setCheckAll: (bool: boolean) => void;
-  selectProduct: any[];
-  setSelectProduct: (value: string[]) => void;
-  search: string;
-  setSearch: (value: string) => void;
-};
+// type StoreState = {
+//   price: number;
+//   SumPrice: (value: number) => void;
+//   MinusPrice: (value: number) => void;
+//   CheckAll: boolean;
+//   setCheckAll: (bool: boolean) => void;
+//   selectProduct: any[];
+//   setSelectProduct: (value: string[]) => void;
+//   search: string;
+//   setSearch: (value: string) => void;
+// };
 
-export const useStoreHook = create<StoreState>((set) => ({
-  price: 0,
-  CheckAll: false,
-  search: "",
-  selectProduct: [],
-  SumPrice: (value: number) => set((state) => ({ price: state.price + value })),
-  MinusPrice: (value: number) =>
-    set((state) => ({ price: state.price - value })),
-  setCheckAll: (bool: boolean) => set(() => ({ CheckAll: bool })),
-  setSelectProduct: (value: string[]) =>
-    set((state) => ({ selectProduct: value })),
-  setSearch: (value: string) => set((state) => ({ search: value })),
-}));
+// export const useStoreHook = create<StoreState>((set) => ({
+//   price: 0,
+//   CheckAll: false,
+//   search: "",
+//   selectProduct: [],
+//   SumPrice: (value: number) => set((state) => ({ price: state.price + value })),
+//   MinusPrice: (value: number) =>
+//     set((state) => ({ price: state.price - value })),
+//   setCheckAll: (bool: boolean) => set(() => ({ CheckAll: bool })),
+//   setSelectProduct: (value: string[]) =>
+//     set((state) => ({ selectProduct: value })),
+//   setSearch: (value: string) => set((state) => ({ search: value })),
+// }));

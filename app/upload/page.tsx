5a75139ca8d97f17842307505d46ad27b065b592
rieliss <133730089+rieliss@users.noreply.github.com>
2023-11-19 12:54:21 +0700

    1.0.4

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,29 +1,32 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { AddImage } from "@/function/addImage";
 import React from "react";
+import { useFormStatus } from "react-dom";
 
 type Props = {};
 
 export default function Upload({}: Props) {
+  function SubmitButton() {
+    const { pending } = useFormStatus();
+    return (
+      <Button type="submit" size="lg">
+        {pending ? "Uploading..." : "Upload"}
+      </Button>
+    );
+  }
   return (
     <form
       action={AddImage}
       className="flex flex-col items-center justify-center gap-y-4 p-24"
     >
-      {/* <input
-        type="text"
-        name="type"
-        className="border-black-300 border-2 rounded"
-      /> */}
       <input
         type="file"
         name="file"
         multiple
         className="border-black-300 border-2 rounded"
       />
-      <Button type="submit" size="lg">
-        Upload
-      </Button>
+      <SubmitButton />
     </form>
   );
 }
--- function/addImage.ts
+++ function/addImage.ts
@@ -8,22 +8,41 @@ export async function AddImage(data: FormData) {
   try {
     const files = data.getAll("file") as File[];
     const type = data.get("type") as string;
-
-    const typeId = await prisma.typeImg.create({
-      data: {
-        type: type,
-      },
-    });
+    // console.log("files : ", files);
+    // console.log("type : ", type);
 
     for (const file of files) {
       const bytes = await file.arrayBuffer();
       const buffer = Buffer.from(bytes);
       const path = join(process.cwd(), "public", "uploads", file.name);
       await writeFile(path, buffer);
+      // console.log("bytes : ", bytes);
+      // console.log("buffer : ", buffer);
+      // console.log("path : ", path);
+      // console.log("file.name : ", file.name.slice(0, 7));
+      const user = await prisma.user.findMany({
+        select: {
+          username: true,
+          name: true,
+          department: true,
+          status: true,
+        },
+        where: {
+          username: file.name.slice(0, 7),
+        },
+      });
+      const typeId = await prisma.typeImg.create({
+        data: {
+          type: file.name.slice(0, 7),
+        },
+      });
       await prisma.imgFile.create({
         data: {
           originalName: file.name,
           authorId: typeId.id as number,
+          username: user[0].username,
+          name: user[0].name,
+          department: user[0].department,
         },
       });
     }
